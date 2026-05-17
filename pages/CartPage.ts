dc17788149bce209bd2433eae83f48f885f7a0a7
prismzdrive <145449829+RadomirTikkerbar@users.noreply.github.com>
2026-05-17 09:41:06 +0300

    Refactor CartPage to remove inheritance and update locators

--- pages/CartPage.ts
+++ pages/CartPage.ts
@@ -1,26 +1,18 @@
 import { Page, Locator, expect } from '@playwright/test';
-import { BasePage } from './BasePage';
 
-export class CartPage extends BasePage {
-  private readonly cartQty: Locator;
-  private readonly cartSubtotals: Locator;
-  private readonly cartTotal: Locator;
-  private readonly removeButton: Locator;
+export class CartPage {
+  // TODO: define locators
 
-  constructor(page: Page) {
-    super(page);
-    this.cartQty = this.page.locator('.order-qty > .o-value');
-    this.cartSubtotals = this.page.locator('.tbl-row > .subtotal');
-    this.cartTotal = this.page.locator('.order-total > .o-value');
-    this.removeButton = this.page.locator('.icon-remove');
+  constructor(private page: Page) {
+    // TODO: initialize locators
   }
 
   async verifyCartCount(expectedCount: number) {
-    await expect(this.cartQty).toContainText(expectedCount.toString());
+    await expect(this.page.locator('.order-qty > .o-value')).toContainText(expectedCount.toString());
   }
 
   async verifyCartSumIsCorrect() {
-    const cartItems = await this.cartSubtotals.all();
+    const cartItems = await this.page.locator('.tbl-row > .subtotal').all();
 
     let cartItemsSum = 0;
 
@@ -30,14 +22,14 @@ export class CartPage extends BasePage {
       cartItemsSum += price;
     }
 
-    const basketSumTotalText = await this.cartTotal.textContent();
-    const basketSumTotal = Number((basketSumTotalText || '').replace(/[^0-9.,]+/g, '').replace(',', '.')) || 0;
+    let basketSumTotalText = await this.page.locator('.order-total > .o-value').textContent();
+    let basketSumTotal = Number((basketSumTotalText || '').replace(/[^0-9.,]+/g, '').replace(',', '.')) || 0;
 
     expect(basketSumTotal).toBeCloseTo(cartItemsSum, 2);
     return cartItemsSum;
   }
 
   async removeItemByIndex(index: number) {
-    await this.removeButton.nth(index).click();
+    await this.page.locator('.icon-remove').nth(index).click();
   }
 }
